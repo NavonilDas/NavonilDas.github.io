40ff58225d5c3c379f864e89accec5713fdd0144
NavonilDas <navneeladas@gmail.com>
2025-06-15 19:36:15 +0530

    New Website on Next Js

--- Portfolio/build.sh
+++
@@ -1,9 +0,0 @@
-#!bin/sh
-
-rm -f ../*.js
-rm -f ../*.css
-rm -f ../*.html
-rm -f ../*.ico
-rm -f ../*.txt
-rm -r -f ../assets
-ng build --prod --aot --extract-css --base-href https://navonildas.github.io/ && cp -a dist/Portfolio/. ../
